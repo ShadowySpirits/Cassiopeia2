dd60aaceecb59626182b5bc744a0deb5535c4b5b
SSpirits <admin@sspirits.top>
2019-05-05 14:52:27 +0800

    Auto clear EditText focus & auto send request

--- app/src/main/java/co/bangumi/cassiopeia/view/LoginActivity.kt
+++ app/src/main/java/co/bangumi/cassiopeia/view/LoginActivity.kt
@@ -1,5 +1,6 @@
 package co.bangumi.cassiopeia.view
 
+import android.view.KeyEvent
 import android.view.View
 import co.bangumi.cassiopeia.R
 import co.bangumi.cassiopeia.databinding.ActivityLoginBinding
@@ -23,6 +24,19 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
     override fun initView() {
         mBinding.presenter = this
         mBinding.vm = mViewModel
+        mBinding.layout.addOnLayoutChangeListener { _, _, _, _, bottom, _, _, _, oldBottom ->
+            if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > 200)) {
+                mBinding.pw.clearFocus()
+                mBinding.user.clearFocus()
+            }
+        }
+        mBinding.pw.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                mBinding.fab.callOnClick()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
     }
 
     override fun getLayoutId(): Int = R.layout.activity_login
